babbf7479d8942a5e5a4c349a5ac043dc8c19f17
Fabien Udriot <fabien@ecodev.ch>
2016-08-22 21:26:24 +0200

    [FEATURE] Hide all modules except the scheduler

---
+++ ext_tables.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('TYPO3_MODE')) {
+    die ('Access denied.');
+}
+
+if (TYPO3_MODE === 'BE') {
+
+    // Default User TSConfig to be added in any case.
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+
+		# Hide the module in the BE.
+		options.hideModules.web := addToList(layout,ViewpageView,list,info,func,ts)
+		options.hideModules.file := addToList(FilelistList)
+		options.hideModules.system := addToList(BeuserTxPermission,BeuserTxBeuser,InstallInstall,BelogLog,dbint,config,ReportsTxreportsm1)
+	');
+
+}
